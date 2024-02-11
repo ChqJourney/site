01f06209ccd48c353b53b36a0f8d1373c89f55ed
ChqJourney <20310381@qq.com>
2024-02-11 22:28:55 +0800

    dd

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,19 +1,10 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
-  },
-
-  preprocess: [vitePreprocess({})],
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
 };
+
